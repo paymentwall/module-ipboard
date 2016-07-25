e1f2a6c882975b24d54689b3aae674957860ce59
David Hoang <doanhkaka@users.noreply.github.com>
2016-07-25 10:46:54 +0700

    Fix pingback error when use proxy (#7)

--- src/applications/nexus/sources/Gateway/Paymentwall/Paymentwall.php
+++ src/applications/nexus/sources/Gateway/Paymentwall/Paymentwall.php
@@ -167,8 +167,8 @@ class _Paymentwall extends \IPS\nexus\Gateway
         $settings = $this->getSettings();
         self::initPaymentwall($settings['project_key'], $settings['secret_key']);
 
-        $pingback = new \Paymentwall_Pingback($_GET, $_SERVER['REMOTE_ADDR']);
-        //$result = array();
+        $pingback = new \Paymentwall_Pingback($_GET, $this->getRealClientIP());
+        
         if ($pingback->validate()) {
             $invoice = $transaction->get_invoice();
 
@@ -194,6 +194,31 @@ class _Paymentwall extends \IPS\nexus\Gateway
         } else {
             return $pingback->getErrorSummary();
         }
+    }
+
+    public function getRealClientIP()
+    {
+        if (function_exists('getallheaders')) {
+            $headers = getallheaders();
+        } else {
+            $headers = $_SERVER;
+        }
+
+        //Get the forwarded IP if it exists
+        if (array_key_exists('X-Forwarded-For', $headers)
+            && filter_var($headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)
+        ) {
+            $the_ip = $headers['X-Forwarded-For'];
+        } elseif (array_key_exists('HTTP_X_FORWARDED_FOR', $headers)
+            && filter_var($headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)
+        ) {
+            $the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+        } elseif(array_key_exists('Cf-Connecting-Ip', $headers)) {
+            $the_ip = $headers['Cf-Connecting-Ip'];
+        } else {
+            $the_ip = filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+        }
+        return $the_ip;
     }
 
     /* !ACP Configuration */
